ef637599042b2c0d4e3d9987a56bdd7ef13484e2
WazeAzure <55005873+WazeAzure@users.noreply.github.com>
2020-09-08 11:44:00 +0800

    Create script.js
    Ini script dnegan tujuan untuk mengganti gambar di bagian-tugas-1, home page.

---
+++ assets/animation/script.js
@@ -0,0 +1,11 @@
+//Ini script jQuery untuk ganti-ganti gambar di utama
+$(document).ready(function trial(){
+  let image_array = ["assets/image/img9.jpg","assets/image/img10.jpg","assets/image/img11.jpg"];
+  curentImageIndex = 0;
+  setInterval(function(){
+    console.log(image_array[curentImageIndex]);
+    var p = $('.welcome-main');
+    p.css("background-image","url("+image_array[curentImageIndex++] + ")",5000);
+    if(curentImageIndex>= image_array.length){curentImageIndex = 0}
+  }, 5000);
+});
