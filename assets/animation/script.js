8ad7c7cfe9e70dcefe3d89263110c32bb1cac319
WazeAzure <55005873+WazeAzure@users.noreply.github.com>
2020-09-09 11:51:51 +0800

    Update script.js

--- assets/animation/script.js
+++ assets/animation/script.js
@@ -1,6 +1,5 @@
 //Ini script jQuery untuk ganti-ganti gambar di utama
-$(window).on("load", function() {
-//$(document).ready(function trial(){
+$(document).ready(function trial(){
   let image_array = ["assets/image/img9.jpg","assets/image/img10.jpg","assets/image/img11.jpg"];
   curentImageIndex = 0;
   setInterval(function(){
@@ -9,4 +8,26 @@ $(window).on("load", function() {
     p.css("background-image","url("+image_array[curentImageIndex++] + ")",5000);
     if(curentImageIndex>= image_array.length){curentImageIndex = 0}
   }, 5000);
+
+  $('#list1').click(function(){
+    for(let i = 1;i<9;i++){
+      $(`#latest${i}`).attr('src',`assets/image/Latest${i}.jpg`);
+    }
+  });
+  $('#list2').click(function(){
+    for(let i = 1;i<9;i++){
+      $(`#latest${i}`).attr('src',`assets/image/Latest2_${i}.jpg`);
+    }
+  });
+  $('#list3').click(function(){
+    for(let i = 1;i<9;i++){
+      $(`#latest${i}`).attr('src',`assets/image/Latest3_${i}.jpg`);
+    }
+  });
+  $('#list4').click(function(){
+    for(let i = 1;i<9;i++){
+      $(`#latest${i}`).attr('src',`assets/image/Latest4_${i}.jpg`);
+    }
+  });
+
 });
